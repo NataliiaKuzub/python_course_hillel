9b557dc36d94b0d742c9026e209476abd3607414
Nataliia Kuzub <45484646+NataliiaKuzub@users.noreply.github.com>
2022-02-07 09:52:05 +0200

    Update Homework_4.py

--- Homework_4.py
+++ Homework_4.py
@@ -1,13 +1,13 @@
 """
-This is program written for finding the max power of two, which will be less than given number
+This is program written for finding the max power of two, which will be no more than given number
 """
 
 N = int(input('Choose your number: '))
 n = 1
 pow = 0
 
-while N > n:
+while N >= n:
     n *= 2
     pow +=1
 else:
-    print(int(n/2), 'is the max power of 2, which is less than your number. It is 2 in the power of', (pow-1))
+    print(int(n/2), 'is the max power of 2, which is no more than your number. It is 2 in the power of', (pow-1))
